b4a8a36f222e3daa2c2ec84cf0b10eb4e526f447
nam <namgh627@naver.com>
2023-05-06 00:55:08 +0900

    docker settings, docker compose settings, app module settings, typeorm settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardModule } from './api/board/board.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    BoardModule,
+    ConfigModule.forRoot({ isGlobal: true }), //
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'my_mysql',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'root',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
